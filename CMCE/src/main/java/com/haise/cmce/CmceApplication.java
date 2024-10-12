d9d267228d4cf10ed07b0f87424bb3fa9751748c
.haise_san <98202347+Haise777@users.noreply.github.com>
2024-10-11 23:32:20 -0300

    initial project files

---
+++ CMCE/src/main/java/com/haise/cmce/CmceApplication.java
@@ -0,0 +1,13 @@
+package com.haise.cmce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmceApplication.class, args);
+    }
+
+}
---
+++ CMCE/src/test/java/com/haise/cmce/CmceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haise.cmce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
